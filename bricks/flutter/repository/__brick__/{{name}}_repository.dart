ef45bf19acf5d2f17adc0af2f3d7d0ce0f3dd217
Michael Frischbutter <33195197+mfrischbutter@users.noreply.github.com>
2022-05-20 15:44:19 +0200

    Update {{name}}_repository.dart

--- bricks/flutter/repository/__brick__/{{name}}_repository.dart
+++ bricks/flutter/repository/__brick__/{{name}}_repository.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final {{#camelCase}}{{name}}{{/camelCase}}RepositoryProvider = Provider((ref) => {{#pascalCase}}{{name}}{{/pascalCase}}Repository(ref.read));
+final {{#camelCase}}{{name}}{{/camelCase}}Repository = Provider((ref) => {{#pascalCase}}{{name}}{{/pascalCase}}Repository(ref.read));
 
 class {{#pascalCase}}{{name}}{{/pascalCase}}Repository {
   {{#pascalCase}}{{name}}{{/pascalCase}}Repository(this._reader) {}
